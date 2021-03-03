b7877bf6ec9a6c0fac78b43bc6c45116efc89327
MadMakZa <immakza@gmail.com>
2021-03-03 23:41:46 +0300

    GlobalScope

--- app/src/main/java/com/examples/coroutinestest/MainActivity.kt
+++ app/src/main/java/com/examples/coroutinestest/MainActivity.kt
@@ -4,18 +4,26 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.widget.TextView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
-
+    val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
+        GlobalScope.launch {
+            delay(3000L)
+            Log.d(TAG, "Coroutine says hello from thread ${Thread.currentThread().name}")
+        }
+        Log.d(TAG, "Hello from thread here ${Thread.currentThread().name}")
+        
 
     }
 
