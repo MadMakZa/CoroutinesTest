1aab79a841fce4e6a3d0099c2e6deadc8012ccdd
MadMakZa <immakza@gmail.com>
2021-03-05 23:55:52 +0300

    Start new Activity with GlobalScope launch

--- app/src/main/java/com/examples/coroutinestest/MainActivity.kt
+++ app/src/main/java/com/examples/coroutinestest/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.examples.coroutinestest
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
@@ -13,35 +15,33 @@ import kotlin.time.measureTime
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
-    lateinit var textView: TextView
+    lateinit var btnStart: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textView = findViewById(R.id.textView)
+        btnStart = findViewById(R.id.buttonStartActivity)
 
-        GlobalScope.launch { (Dispatchers.IO)
-            val time = measureTimeMillis {
-                val answer1 = async { networkCall1() }
-                val answer2 = async { networkCall2() }
-                Log.d(TAG, "Answer1 is: ${answer1.await()}")
-                Log.d(TAG, "Answer2 is: ${answer2.await()}")
+        btnStart.setOnClickListener {
+            GlobalScope.launch {
+                while (true){
+                    delay(1000L)
+                    Log.d(TAG, "Still running...")
+                }
             }
-            Log.d(TAG, "Request took $time ms.")
+            GlobalScope.launch {
+                delay(5000L)
+                Log.d(TAG, "Start Activity")
+                Intent(this@MainActivity, SecondActivity::class.java).also {
+                    startActivity(it)
+                    finish()
+                }
+            }
         }
-
     }
 
-    suspend fun networkCall1(): String {
-        delay(3000L)
-        return "Answer 1"
-    }
 
-    suspend fun networkCall2(): String {
-        delay(3000L)
-        return "Answer 2"
-    }
 
 
 
