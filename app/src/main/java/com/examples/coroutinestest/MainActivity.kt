945e0b2866cc891bb98ec98e6b1c19136dc4588c
MadMakZa <immakza@gmail.com>
2021-03-05 23:39:35 +0300

    suspend funs with async

--- app/src/main/java/com/examples/coroutinestest/MainActivity.kt
+++ app/src/main/java/com/examples/coroutinestest/MainActivity.kt
@@ -23,14 +23,10 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch { (Dispatchers.IO)
             val time = measureTimeMillis {
-                var answer1: String? = null
-                var answer2: String? = null
-                val job1 = launch { answer1 = networkCall1() }
-                val job2 = launch { answer2 = networkCall2() }
-                job1.join()
-                job2.join()
-                Log.d(TAG, "Answer1 is: $answer1")
-                Log.d(TAG, "Answer2 is: $answer2")
+                val answer1 = async { networkCall1() }
+                val answer2 = async { networkCall2() }
+                Log.d(TAG, "Answer1 is: ${answer1.await()}")
+                Log.d(TAG, "Answer2 is: ${answer2.await()}")
             }
             Log.d(TAG, "Request took $time ms.")
         }
